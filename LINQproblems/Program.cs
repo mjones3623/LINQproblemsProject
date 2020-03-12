9c002f30d50ad05c33e1a0b4384927e5818883fd
Mathew Jones <anilox123@gmail.com>
2020-03-12 07:58:08 -0500

    Only #1 and #2 done

--- LINQproblems/Program.cs
+++ LINQproblems/Program.cs
@@ -10,6 +10,54 @@ namespace LINQproblems
     {
         static void Main(string[] args)
         {
+
+
+            //LINQ problems, #1
+
+            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+
+            //var wordsWithTh = words.Where(s => s.Contains("th")).ToList();
+
+            //Console.WriteLine("Hello");
+
+
+            //LINQ problem, #2 
+
+            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+            //var withoutDuplicates = names.Distinct().ToList();
+
+
+
+            //LINQ problem, #3   ////NOT DONE////
+            //List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+
+            //foreach (var s in classGrades.Split(','))
+            //{
+            //    int num;
+            //    if (int.TryParse(s, out num))
+            //        yield return num;
+
+
+            //    List<int> intList = classGrades.ConvertAll<int>(Convert.ToInt32);
+
+                //List<int> classGrades = 
+
+
+                //for (int i = 0; i < classGrades.Count; i++)
+                //{
+                //    intList.Add(int.Parse(classGrades[i]));
+
+                //LINQ problem, #4   ////NOT DONE////
+
+
+
+            }
         }
+       
+
+
+       
     }
 }
